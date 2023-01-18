2031c073930eaf08b9b2f7d2f6bdf6ff4c5d65de
Njeri Muchiri <muchirinjeri3210@gmail.com>
2023-01-18 12:42:45 +0300

    Added child templates

--- app/views.py
+++ app/views.py
@@ -4,9 +4,9 @@ from flask import render_template
 
 @app.route('/')
 @app.route('/home')
-def index():
+def index_page():
     return render_template('public/index.html')
 
 @app.route('/about')
-def about():
+def about_page():
     return "<h1 style='Color: #ffc87c'>About</h1>"
