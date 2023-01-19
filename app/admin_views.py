a6fd3bb66a2235b4ff19e6efa549e1fc27b0bdab
Njeri Muchiri <muchirinjeri3210@gmail.com>
2023-01-19 12:59:40 +0300

    adding new div

--- app/admin_views.py
+++ app/admin_views.py
@@ -1,10 +1,13 @@
 from app import app
 from flask import render_template
 
+@app.route('/admin')
+def admin_page():
+    return render_template('/admin/dashboard.html')
 @app.route('/admin/dashboard')
 def admin_dashboard():
     return render_template('/admin/dashboard.html')
 
 @app.route('/admin/profile')
 def admin_profile():
-    return "<h1 style='Color:#ed872d'>Admin profile</h1>"
+    return render_template('/admin/admin_profile.html')
