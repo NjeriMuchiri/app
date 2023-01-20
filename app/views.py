4aac442deb3871da7505ef40b9fe0d36c8ae2c4b
Njeri Muchiri <muchirinjeri3210@gmail.com>
2023-01-20 11:00:55 +0300

    Added a new class and methods

--- app/views.py
+++ app/views.py
@@ -10,7 +10,38 @@ def index_page():
 @app.route('/jinja')
 def jinja():
     my_name = "Kashee"
-    return render_template('public/jinja.html', my_name=my_name)
+    age = 23
+    langs = ['React','Cprograme','Python','Javascript']
+    friends = {
+        'Kaggy': 20,
+        'Kawambo': 30,
+        'kashii': 27,
+        'Ken': 27, 
+    }
+    colours = ('Brown', 'Gray')
+    
+    cool = True
+    class GitRepos:
+        def __init__(self, name, description, url):
+            self.name = name
+            self.description = description
+            self.url = url
+        def pull(self):
+            return f'Pulling repos {self.name}'
+        def clone(self):
+            return f'Cloning into {self.url}'
+       
+    gitRemote = GitRepos(
+        name = "FlaskJinja",
+        description="Learning and mastering",
+        url = "https://github.com/NjeriMuchiri"
+    )   
+    def repeat(x,qty):
+            return x * qty
+    
+        
+    return render_template('public/jinja.html',
+                           my_name=my_name,age=age,langs=langs,colours=colours,cool=cool,GitRepos=GitRepos,gitRemote=gitRemote,repeat=repeat)
 
 @app.route('/about')
 def about_page():
